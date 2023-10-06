1a9a13fc85b75fa70eec6d01349ab19800e7308d
Matteo Murtas [Carimali] <m.murtas@carimali.com>
2023-10-06 15:00:47 +0200

    Button to change screen orientation

--- app/src/main/java/com/carimali/testdisplayres/MainActivity.java
+++ app/src/main/java/com/carimali/testdisplayres/MainActivity.java
@@ -1,9 +1,12 @@
 package com.carimali.testdisplayres;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +14,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        findViewById(R.id.btnOrientationLandscape).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+        });
+
+        findViewById(R.id.btnOrientationPortrait).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        });
     }
 
     @Override
